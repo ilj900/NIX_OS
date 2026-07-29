21a6f11e8b8355f3c62b95046d41b65f280a7289
ilj900 <ilj900@gmail.com>
2026-07-29 23:18:53 +0200

    Moving to flakes

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.ExMachina = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        /etc/nixos/hardware-configuration.nix
+      ];
+    };
+  };
+}
