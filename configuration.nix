1f626b278a6e85d8f0319fe40a181939a5a80d73
ilj900 <ilj900@gmail.com>
2026-07-29 12:19:06 +0200

    Limit to 3 configs

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
